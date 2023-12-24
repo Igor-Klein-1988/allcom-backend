d8010a3b638aa150a1c0ff41d1d7f9ce78b91cff
Igor Klein <igorklein1988@gmail.com>
2023-12-24 18:50:13 +0100

    create app

---
+++ src/main/java/de/allcom/AllcomApplication.java
@@ -0,0 +1,13 @@
+package de.allcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllcomApplication.class, args);
+	}
+
+}
