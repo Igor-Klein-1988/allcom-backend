a5785157571f6bbb23d42567a1c8736879496b30
Volodymyr Maiorov <maiorov.volodymyr.v@gmail.com>
2024-01-09 00:24:06 +0100

    add jwt, modify entity User, create entity Role,Address, Token. Modify liquidbase files

--- src/main/java/de/allcom/controllers/api/UsersApi.java
+++ src/main/java/de/allcom/controllers/api/UsersApi.java
@@ -1,36 +1,18 @@
 package de.allcom.controllers.api;
 
-import de.allcom.dto.StandardResponseDto;
+
 import de.allcom.dto.user.UserDto;
-import de.allcom.dto.user.UserRegistrationDto;
-import de.allcom.validation.dto.ValidationErrorsDto;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
 
 @Tags(@Tag(name = "Users"))
 @RequestMapping("/api/users")
 public interface UsersApi {
-    @Operation(summary = "User Registration", description = "Available to everyone. Default role is Client")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "User registered", content =
-                @Content(mediaType = "application/json", schema = @Schema(implementation = UserDto.class))),
-            @ApiResponse(responseCode = "400", description = "Validation error", content =
-                @Content(mediaType = "application/json", schema = @Schema(implementation = ValidationErrorsDto.class))),
-            @ApiResponse(responseCode = "409", description = "User with this email already exists", content =
-                @Content(mediaType = "application/json", schema = @Schema(implementation = StandardResponseDto.class)))}
-    )
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/register")
-    UserDto register(@RequestBody @Valid UserRegistrationDto request);
+
+    @Operation(summary = "Get all users")
+    List<UserDto> getAll();
 }
